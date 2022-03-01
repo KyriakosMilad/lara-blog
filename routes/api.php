f06597458428de7359426ab8480a847a8334472c
Kyriakos Milad <contact.kyriakos@gmail.com>
2022-03-01 07:58:01 +0200

    Register Post routes

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,18 @@ Route::get('/', function () {
     return "lara-blog";
 });
 
+// auth
 Route::post('/register', 'register@AuthController');
 Route::post('/login', 'login@AuthController');
+
+// reading posts
+Route::get('/posts', 'index@PostController');
+Route::get('/posts/{id}', 'show@PostController');
+
+Route::middleware(['apiauth', 'admin'])->group(function () {
+    // control posts
+    Route::post('/posts', 'store@PostController');
+    Route::put('/posts/{id}', 'update@PostController');
+    Route::delete('/posts/{id}', 'destory@PostController');
+});
+
