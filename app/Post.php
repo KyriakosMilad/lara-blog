93af6d7529299f31962e3541e63d7183f6f33d57
Kyriakos Milad <contact.kyriakos@gmail.com>
2022-03-05 08:32:57 +0200

    Setup post model relations

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ["title", "body"];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany('App\Reply');
+    }
 }
